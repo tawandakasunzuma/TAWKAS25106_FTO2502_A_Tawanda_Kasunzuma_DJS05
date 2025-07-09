a456a33febb60dd8d477ee7cd8618453d8d33a37
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-09 19:28:04 +0200

    create state for filter, sort, search and page state. Pass states to their children

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,19 @@ import "./styles/App.css"
 
 export default function App() {
 
+  // State for fetch logic
   const [podcastData,setPodcastData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [hasError,setHasError] = useState(false);
+
+  // State for search, filter, sort, pagination
+  const [searchLetters,setSearchLetters] = useState("");
+  const [selectedGenre,setSelectedGenre] = useState("All-Genres");
+  const [sortOrder,setSortOrder] = useState("date-desc");
+  const [currentPage,setCurrentPage] = useState(1);
+
+  // Podcasts displayed per page
+  const podcastsPerPage = 12;
 
   useEffect(() => {
 
@@ -29,7 +39,7 @@ export default function App() {
       // Show error in console and on the UI
       .catch(error => {
         console.error("Error:", error);
-        setHasError(error);
+        setHasError(true);
       })
   
       // Stop loading
@@ -38,10 +48,33 @@ export default function App() {
       })
   },[])
 
+  // Check state updates when values change
+  useEffect(() => {
+    console.log({ searchLetters, selectedGenre, sortOrder, currentPage});
+  }, [searchLetters, selectedGenre, sortOrder, currentPage]);
+
   return (
     <>
-      <Header />
-      <Main podcastData={podcastData} />
+      <Header 
+        searchLetters={searchLetters} 
+        setSearchLetters={setSearchLetters}
+      />
+
+      <Main 
+        podcastData={podcastData}
+
+        searchLetters={searchLetters}
+
+        selectedGenre={selectedGenre}
+        setSelectedGenre={setSelectedGenre}
+        sortOrder={sortOrder}
+        setSortOrder={setSortOrder}
+
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+
+        podcastsPerPage={podcastsPerPage}
+      />
 
       {hasError && 
         <div className='error-container'>

4378c2f768fd292c22ac33f2ea01df0e2c0b32f7
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-11 14:38:02 +0200

    add pagination component and add comments

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,31 @@
 import "../styles/Main.css"
 import FilterSection from "./FilterSection"
 import CardSection from "./CardSection"
+import Pagination from "./Pagination"
 
 export default function Main (props) {
     return (
         <main>
+            {/* Filter section */}
             <FilterSection 
                 selectedGenre={props.selectedGenre} 
                 setSelectedGenre={props.setSelectedGenre} 
                 sortOrder={props.sortOrder} 
                 setSortOrder={props.setSortOrder} 
+                setCurrentPage={props.setCurrentPage}
             />
 
+            {/* Card section */}
             <CardSection podcastData={props.podcastData} />
+
+            {/* Pagination section */}
+            {props.totalPages > 0 && (
+                <Pagination
+                    currentPage={props.currentPage}
+                    totalPages={props.totalPages}
+                    setCurrentPage={props.setCurrentPage}
+                />
+            )}
         </main>
     )
 }

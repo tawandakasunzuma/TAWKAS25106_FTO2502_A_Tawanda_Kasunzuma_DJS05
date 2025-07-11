193aae4944a52609269d0d3cdfcd0f7cb46938c0
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-11 14:30:16 +0200

    add comments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,22 +7,31 @@ export default function Header ({searchLetters,setSearchLetters}) {
 
     return (
         <header>
+
+            {/* Name of podcast website */}
             <div className="header-left">
                 <img className="icon-1" src={podcastIcon} alt="Podcast icon" />
                 <h1 className="app-name">Podcast App</h1>
             </div>
+
             <div className="header-right">
+                
+                {/* Search bar */}
                 <input 
                     className="search-bar" 
                     type="text" 
                     placeholder="Search..."
                     value={searchLetters}
                     onChange={event => setSearchLetters(event.target.value)} />
+
+                {/* Search icon image */}
                 <img
                     className="search-icon icon-2"
                     src={searchIcon}
                     alt="Search icon"
                 />
+
+                {/* Profile picture icon */}
                 <img
                     className="icon-3"
                     src={accountCircle}

e687bf26e44e8b14b218dbbe20c74299a546d745
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-11 14:34:11 +0200

    add return to first page logic and comments

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,8 +1,9 @@
 import "../styles/FilterSection.css"
 import genres from "../assets/genres"
 
-export default function FilterSection ({selectedGenre,setSelectedGenre,sortOrder,setSortOrder}) {
+export default function FilterSection ({selectedGenre,setSelectedGenre,sortOrder,setSortOrder,setCurrentPage}) {
 
+  // Create genre options from API data
   const genreList = genres.map(genre => (
     <option value={genre.id} key={genre.id}>{genre.title}</option>
   ));
@@ -11,12 +12,16 @@ export default function FilterSection ({selectedGenre,setSelectedGenre,sortOrder
     <section className="filter-section">
 
       <p className="filter-by-text">Filter by:</p>
-
+      
+      {/* Genre select menu */}
       <select 
         name="all-genres" 
         className="all-genres" 
         value={selectedGenre || "All Genres"}
-        onChange={event => setSelectedGenre(event.target.value)}
+        onChange={event => {
+          setSelectedGenre(event.target.value);
+          setCurrentPage(1);
+        }}
       >
 
         <option value="All Genres">All Genres</option>
@@ -24,15 +29,19 @@ export default function FilterSection ({selectedGenre,setSelectedGenre,sortOrder
       
       </select>
       
+      {/* Sorting select menu */}
       <select 
         name="recently-updated" 
         className="recently-updated"
         value={sortOrder}
-        onChange={event => setSortOrder(event.target.value)}
+        onChange={event => {
+          setSortOrder(event.target.value)
+          setCurrentPage(1);
+        }}
       >
 
-        <option value="Newest">Recently Updated (Newest)</option>
-        <option value="Oldest">Last Updated (Oldest)</option>
+        <option value="Newest">Newest</option>
+        <option value="Oldest">Oldest</option>
         <option value="TitleAsc">Title (A-Z)</option>
         <option value="TitleDesc">Title (Z -A)</option>
       

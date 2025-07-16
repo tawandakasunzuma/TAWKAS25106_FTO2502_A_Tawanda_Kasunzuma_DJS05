64cf7fc09bbbff8ed65acec55f604ff67d8b120b
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-16 14:07:34 +0200

    create ShowDetail component and add Link tags to wrap podcast cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import "../styles/Card.css";
 import seasonsIcon from '../assets/images/seasons-icon.svg';
 import genreList from "../assets/genres";
+import { Link } from "react-router-dom"
 
 export default function Card ({podcastData}) {
 
@@ -41,32 +42,34 @@ export default function Card ({podcastData}) {
     const formattedDate = getDate(podcastData);
 
     return (
-        <article className="podcast-container">
-            
-            {/* Image container */}
-            <div className="image-container">
-                <img className="podcast-image" src={podcastData.image} alt={`${podcastData.title} cover image`} loading="lazy" />
-                <p className="podcast-cover-title">{podcastData.title}</p>
-            </div>
-            
-            {/* Podcast title */}
-            <h3 className="podcast-title">{podcastData.title}</h3>
-            
-            {/* Seasons section */}
-            <div className="seasons-section">
-                <img className="seasons-icon" src={seasonsIcon} alt="Seasons icon" />
-                <p className="num-seasons">{`${podcastData.seasons} seasons`}</p>
-            </div>
+        <Link to={`/show/${podcastData.id}` className="card-link">
+            <article className="podcast-container">
+                
+                {/* Image container */}
+                <div className="image-container">
+                    <img className="podcast-image" src={podcastData.image} alt={`${podcastData.title} cover image`} loading="lazy" />
+                    <p className="podcast-cover-title">{podcastData.title}</p>
+                </div>
+                
+                {/* Podcast title */}
+                <h3 className="podcast-title">{podcastData.title}</h3>
+                
+                {/* Seasons section */}
+                <div className="seasons-section">
+                    <img className="seasons-icon" src={seasonsIcon} alt="Seasons icon" />
+                    <p className="num-seasons">{`${podcastData.seasons} seasons`}</p>
+                </div>
 
-            {/* Genre section */}
-            <div className="genre-section">
-                {genreTags}
-            </div>
+                {/* Genre section */}
+                <div className="genre-section">
+                    {genreTags}
+                </div>
 
-            {/* Last updated section */}
-            <p className="updated-status">
-                {formattedDate}
-            </p>
-        </article>
+                {/* Last updated section */}
+                <p className="updated-status">
+                    {formattedDate}
+                </p>
+            </article>
+        </Link>
     )
 }
---
+++ src/components/ShowDetail.jsx
@@ -0,0 +1,8 @@
+export default function ShowDetail () {
+    return (
+        <>
+            {/* Show details of podcast clicked */}
+            <h1>Podcast details page</h1>
+        </>
+    )
+}
